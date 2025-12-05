ef1f4678f5487c377566773516f0fd0a12866dfc
Mariusz Macheta <mmacheta@future-processing.com>
2025-12-05 08:28:04 +0100

    Day05

---
+++ AdventOfCode2025/Day05/Program.cs
@@ -0,0 +1,49 @@
+﻿using Utils;
+
+var input = File.ReadAllLines("input-test.txt");
+
+var ranges = input.TakeWhile(i => !string.IsNullOrWhiteSpace(i))
+	.Select(line =>
+	{
+		var parts = line.Split("-");
+		return new LongRange(long.Parse(parts[0]), long.Parse(parts[1]));
+	})
+	.ToList();
+
+var numbers = input.SkipWhile(i => !string.IsNullOrWhiteSpace(i)).Skip(1)
+	.Select(long.Parse)
+	.ToList();
+
+var result1 = numbers
+	.Where(n => ranges.Any(r => r.Contains(n)))
+	.Count();
+
+Console.WriteLine("1: " + result1);
+
+var uniformRanges = new List<LongRange>();
+ranges.Sort((a, b) => a.start.CompareTo(b.start));
+var currentRange = ranges[0];
+
+for (int i = 1; i < ranges.Count; i++)
+{
+	var nextRange = ranges[i];
+	var merged = currentRange.Merge(nextRange).ToList();
+
+	if (merged.Count == 1)
+	{
+		currentRange = merged[0];
+	}
+	else
+	{
+		uniformRanges.Add(currentRange);
+		currentRange = merged[1];
+	}
+}
+
+uniformRanges.Add(currentRange);
+
+var result2 = uniformRanges
+	.Select(r => r.Length)
+	.Sum();
+
+Console.WriteLine("2: " + result2);
--- AdventOfCode2025/Utils/Range.cs
+++ AdventOfCode2025/Utils/Range.cs
@@ -1,6 +1,25 @@
 ﻿namespace Utils;
 
-public record IntRange(int start, int end)
+public record LongRange(long start, long end)
 {
-	public bool Contains(int number) => start <= number && number <= end;
+	public long Length => end - start + 1;
+
+	public bool Contains(long number) => start <= number && number <= end;
+	public bool Contains(LongRange other) => start <= other.start && other.end <= end;
+	public bool Overlaps(LongRange other) => start <= other.end && other.start <= end;
+	public bool IsAdjacent(LongRange other) => end + 1 == other.start || other.end + 1 == start;
+
+	public IEnumerable<LongRange> Merge(LongRange other)
+	{
+		if (!Overlaps(other) && !IsAdjacent(other))
+		{
+			yield return this;
+			yield return other;
+			yield break;
+		}
+
+		var newStart = Math.Min(start, other.start);
+		var newEnd = Math.Max(end, other.end);
+		yield return new LongRange(newStart, newEnd);
+	}
 }
