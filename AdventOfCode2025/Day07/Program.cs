4817e8d43ab986f9118449ac0b57ad222f4b5427
Mariusz Macheta <mmacheta@future-processing.com>
2025-12-07 10:10:03 +0100

    Day07

---
+++ AdventOfCode2025/Day07/Program.cs
@@ -0,0 +1,34 @@
+﻿
+var lines = File.ReadAllLines("input.txt");
+
+var start = lines[0]
+	.Select((c, i) => new { c, i })
+	.First(p => p.c == 'S').i;
+
+HashSet<Beam> beams2 = [new Beam(start, 0, 1)];
+foreach (var line in lines.Skip(1))
+{
+	beams2 = Split2(beams2, line).ToHashSet();
+}
+
+Console.WriteLine("1: " + beams2.Sum(b => b.splitCounts));
+Console.WriteLine("2: " + beams2.Sum(b => b.possiblePaths));
+
+IEnumerable<Beam> Split2(IEnumerable<Beam> beams, string line)
+{
+	return beams
+		.SelectMany(beam =>
+		{
+			if (line[beam.position] == '^')
+			{
+				var b1 = new Beam(beam.position - 1, beam.splitCounts + 1, beam.possiblePaths);
+				var b2 = new Beam(beam.position + 1, 0, beam.possiblePaths);
+				return new List<Beam>() { b1, b2 };
+			}
+			return [beam];
+		})
+		.GroupBy(b => b.position)
+		.Select(g => new Beam(g.Key, g.Sum(gb => gb.splitCounts), g.Sum(gb => gb.possiblePaths)));
+}
+
+record Beam(int position, decimal splitCounts, decimal possiblePaths);
